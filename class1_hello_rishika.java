39f87501e5bddce4354bbb22ef4004b6157c680a
rishikagupta2437 <rishikagupta2013@gmail,com>
2018-01-17 17:57:58 +0530

    class1 assingment submitt

---
+++ class1_hello_rishika.java
@@ -0,0 +1,8 @@
+package class1;
+
+public class class1_hello_rishika {
+	public static void main(String[] args) {
+		
+		System.out.print("hello world");
+	}
+}
---
+++ class1_rishikag.java
@@ -0,0 +1,9 @@
+package class1;
+
+public class class1_rishikag {
+
+public static void main(String[] args) {
+	
+	System.out.print("Rishika gupta");
+}
+}
